bcf1dd7ac20ec206e2ca9350a4cce98ad82bf2d9
dochi2350 <mathematics0909@gmail.com>
2022-04-04 22:27:19 +0900

    0.0.1

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main()
 {
     char input[10003];
     char variables[1003][1003];
-    int variablesvalue[1003] = {1, };
+    int variablesvalue[1003] = {0, };
 
     scanf("%s", input); //코드 입력
     int inputlength = strlen(input); //코드 길이
@@ -13,9 +14,7 @@ int main()
     int i, codeerror = 0;
     int variablecnt = 0;
 
-    for(i = 0; i < 1003; i++) variablesvalue[i] = 1;
-
-    for(int i = 0; i < inputlength; i++) { //코드 처음부터 끝까지 한글자씩 확인
+    for(i = 0; i < inputlength; i++) { //코드 처음부터 끝까지 한글자씩 확인
         if(input[i] == 73) { //i번째 글자가 대문자 i(I)라면...
             if(input[i + 1] != 33) { //만약 i+1번째 글짜가 느낌표(!)가 아닌 다른 글자라면
                 codeerror = 1; //코드에러
@@ -29,6 +28,14 @@ int main()
             printf("%s\n", variables[variablecnt]);
             int newvariablelength = strlen(variables[variablecnt++]);
             printf("%d\n", newvariablelength);
+
+            int k, variablewhere = 0;
+            for(k = newvariablelength; k > 0; k--) {
+                if(variables[k] == 41) variablewhere += pow(2, k);
+                else if(variable[k] == 40) variablewhere += 0;
+                else codeerror = 1;
+            }
+            if(codeerror == 1) break;
         }
     }
     if(codeerror == 1) printf("error\n");

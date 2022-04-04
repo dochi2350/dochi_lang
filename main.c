a3dcda52483b240bcc63018b0f6b85b9713fb36d
dochi2350 <mathematics0909@gmail.com>
2022-04-04 22:49:24 +0900

    0.0.1

--- main.c
+++ main.c
@@ -15,31 +15,50 @@ int main()
     int variablecnt = 0;
 
     for(i = 0; i < inputlength; i++) { //코드 처음부터 끝까지 한글자씩 확인
-        if(input[i] == 73) { //i번째 글자가 대문자 i(I)라면...
-            if(input[i + 1] != 33) { //만약 i+1번째 글짜가 느낌표(!)가 아닌 다른 글자라면
+        if(input[i] == 73) { //i번째 글자가 대문자 i(I)라면... 한개라면 입력
+            if(input[i + 1] == 73) { //만약 i+1번째 글자가 대문자 i(I)라면... 두개라면 출력
+                int j = 0;
+                while(input[i + 3] != 33) {
+                    variables[variablecnt][j++] = input[i + 3];
+                    i++;
+                }
+                int newvariablelength = strlen(variables[variablecnt]);
+
+                int k, variablewhere = 0;
+                for(k = newvariablelength - 1; k >= 0; k--) {
+                    if(variables[variablecnt][k] == 41) variablewhere += pow(2, newvariablelength - k - 1);
+                    else if(variables[variablecnt][k] == 40) variablewhere += 0;
+                    else codeerror = 1;
+                }
+                if(codeerror == 1) break;
+                printf("%d", variablesvalue[variablewhere]);
+            }
+            else if(input[i + 1] != 33) { //만약 i+1번째 글자가 느낌표(!)가 아닌 다른 글자라면
                 codeerror = 1; //코드에러
                 break; //컴파일 종료
             }
-            int j = 0;
-            while(input[i + 2] != 33) {
-                variables[variablecnt][j++] = input[i + 2];
-                i++;
-            }
-            printf("%s\n", variables[variablecnt]);
-            int newvariablelength = strlen(variables[variablecnt]);
-            printf("len %d\n", newvariablelength);
+            else { //입력
+                int j = 0;
+                while(input[i + 2] != 33) {
+                    variables[variablecnt][j++] = input[i + 2];
+                    i++;
+                }
+                //printf("%s\n", variables[variablecnt]);
+                int newvariablelength = strlen(variables[variablecnt]);
+                //printf("len %d\n", newvariablelength);
 
-            int k, variablewhere = 0;
-            for(k = newvariablelength - 1; k >= 0; k--) {
-                if(variables[variablecnt][k] == 41) variablewhere += pow(2, newvariablelength - k - 1);
-                else if(variables[variablecnt][k] == 40) variablewhere += 0;
-                else codeerror = 1;
-                printf("k %d %d %c\n", k, variablewhere, variables[variablecnt][k]);
+                int k, variablewhere = 0;
+                for(k = newvariablelength - 1; k >= 0; k--) {
+                    if(variables[variablecnt][k] == 41) variablewhere += pow(2, newvariablelength - k - 1);
+                    else if(variables[variablecnt][k] == 40) variablewhere += 0;
+                    else codeerror = 1;
+                    //printf("k %d %d %c\n", k, variablewhere, variables[variablecnt][k]);
+                }
+                if(codeerror == 1) break;
+                variablesvalue[variablewhere] = 1;
+                variablecnt++;
+                //printf("where %d %d\n", variablewhere, variablesvalue[variablewhere]);
             }
-            if(codeerror == 1) break;
-            variablesvalue[variablewhere] = 1;
-            variablecnt++;
-            printf("where %d %d\n", variablewhere, variablesvalue[variablewhere]);
         }
     }
     if(codeerror == 1) printf("error\n");
